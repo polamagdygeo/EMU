6afa8c4d366c9538cbe8eb688f7ef225494dd7b9
Pola Magdy <pola@garment.io>
2020-09-08 18:19:29 +0200

    Fixes and performance enhancements

--- emulated_eeprom.c
+++ emulated_eeprom.c
@@ -1,26 +1,31 @@
-/*
- * emulated_eeprom.c
- *
- *  Created on: Aug 24, 2020
- *      Author: Pola
+/**
+ ******************************************************************************
+ * @file           : emulated_eeprom.c
+ * @brief          : Emulated EEPROM module
+ ******************************************************************************
  */
 
+/** Includes ------------------------------------------------------------------*/
 #include "Flash.h"
+#include <string.h>
 
+/* Private macro -------------------------------------------------------------*/
 #define FLASH_END_ADDR										0x8020000
 #define SECTORS_NO											1u
 #define FLASH_PAGES_PER_SECTOR								6u
 #define FLASH_EMU_EE_START									(FLASH_END_ADDR - (SECTORS_NO * FLASH_PAGES_PER_SECTOR * FLASH_PAGE_SIZE))
 
 typedef enum{
-	PAGE_STATUS_INACTIVE,
 	PAGE_STATUS_ACTIVE,
 	PAGE_STATUS_ERASED = 0xffff
 }tPageStatus;
 
-typedef struct{
-	uint16_t page_status;
-	uint16_t place_holder;
+typedef union{
+	struct{
+		uint16_t page_status;
+		uint16_t place_holder;
+	};
+	uint32_t value;
 }tPageHeader;
 
 typedef struct{
@@ -47,8 +52,13 @@ typedef struct{
 	uint8_t active_page_no;
 }tSector_RuntimeInfo;
 
-static tSector_RuntimeInfo sectors_runtime_info_arr[SECTORS_NO] = {[0] = {UINT32_MAX,UINT8_MAX}};
+/* Private variables ---------------------------------------------------------*/
+static tSector_RuntimeInfo sectors_runtime_info_arr[SECTORS_NO] = {[0] = {0,UINT8_MAX}};
 
+/* Private function prototypes -----------------------------------------------*/
+/*** @defgroup
+  * @{
+  */
 static uint32_t Emulated_EEPROM_GetPagesFirstEmptyLocAddr(uint8_t sector_no,uint8_t page_no);
 static uint8_t Emulated_EEPROM_CopyDataToNextPage(uint8_t sector_no,uint8_t page_no,tDataEntry *p_entry);
 static uint8_t Emulated_EEPROM_SwapToNextPage(uint8_t sector_no,uint8_t curr_page_no,tDataEntry *p_entry);
@@ -66,18 +76,18 @@ static uint32_t Emulated_EEPROM_GetPagesFirstEmptyLocAddr(uint8_t sector_no,uint
 {
 	uint32_t ret = 0;
 	uint32_t data_entry_itr = 0;
-	tDataEntry *old_page_data_entry_base_ptr = (tDataEntry*)(GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no) + sizeof(tPageHeader));
+	tDataEntry *data_entry_base_ptr = ((tPage*)GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no))->data_entries_arr;
 
 	for(data_entry_itr = 0 ; data_entry_itr < PAGE_DATA_ENTRIES_MAX_NO ; data_entry_itr++)
 	{
-		if(old_page_data_entry_base_ptr->logical_addr == UINT16_MAX)
+		if(data_entry_base_ptr->logical_addr == UINT16_MAX)
 		{
-			ret = (uint32_t)old_page_data_entry_base_ptr;
+			ret = (uint32_t)data_entry_base_ptr;
 			break;
 		}
 		else
 		{
-			old_page_data_entry_base_ptr++;
+			data_entry_base_ptr++;
 		}
 	}
 
@@ -92,13 +102,17 @@ static uint32_t Emulated_EEPROM_GetPagesFirstEmptyLocAddr(uint8_t sector_no,uint
 static uint8_t Emulated_EEPROM_CopyDataToNextPage(uint8_t sector_no,uint8_t page_no,tDataEntry *p_entry)
 {
 	uint8_t ret = 0;
-	tPage temp_page = {.page_header = {0},.data_entries_arr = {*p_entry}};
+	tPage temp_page = {.page_header = {.value = 0}};
 	tDataEntry *temp_page_top_entry_ptr = temp_page.data_entries_arr + 1;
 	tDataEntry *temp_page_entry_itr_ptr;
-	tDataEntry *old_page_data_entry_end_ptr = (tDataEntry*)(GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no) + sizeof(tPage));
-	tDataEntry *old_page_data_entry_base_ptr = (tDataEntry*)(GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no) + sizeof(tPageHeader));
+	tDataEntry *old_page_data_entry_end_ptr = &((tPage*)GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no))->data_entries_arr[PAGE_DATA_ENTRIES_MAX_NO - 1];
+	tDataEntry *old_page_data_entry_base_ptr = ((tPage*)GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no))->data_entries_arr;
 	uint8_t any_match = 0;
 	tDataEntry *flashing_addr_ptr;
+	uint8_t next_page_no = GET_NEXT_PAGE_NO(page_no);
+
+	memset(temp_page.data_entries_arr,0xff,sizeof(tDataEntry) * PAGE_DATA_ENTRIES_MAX_NO);
+	temp_page.data_entries_arr[0] = *p_entry;
 
 	while(old_page_data_entry_end_ptr >= old_page_data_entry_base_ptr)
 	{
@@ -136,32 +150,46 @@ static uint8_t Emulated_EEPROM_CopyDataToNextPage(uint8_t sector_no,uint8_t page
 		}
 	}
 
-	/*Get next page address*/
-	flashing_addr_ptr = (tDataEntry*)(GET_SECTORS_PAGE_BASE_ADDR(sector_no,GET_NEXT_PAGE_NO(page_no)) + sizeof(tPageHeader));
+	flashing_addr_ptr = (tDataEntry*)(GET_SECTORS_PAGE_BASE_ADDR(sector_no,next_page_no));
 
-	temp_page_entry_itr_ptr = temp_page.data_entries_arr;
-
-	/*Program data entries in the temp page*/
-	Flash_Unlock();
-	while(temp_page_entry_itr_ptr < temp_page_top_entry_ptr)
+	if(((tPageHeader*)flashing_addr_ptr)->value != UINT32_MAX ||
+			(memcmp(((tPage*)flashing_addr_ptr)->data_entries_arr,temp_page.data_entries_arr,sizeof(tDataEntry) * PAGE_DATA_ENTRIES_MAX_NO)))
 	{
-		ret = Flash_Program((uint32_t)flashing_addr_ptr,*((uint32_t*)temp_page_entry_itr_ptr),sizeof(tDataEntry) / 2);
+		/*Erase new page to make sure no data was written in it*/
+		ret = Flash_Erase((uint32_t)flashing_addr_ptr,1);
 
-		if(ret == 1)
+		if(ret)
 		{
-			temp_page_entry_itr_ptr++;
-			flashing_addr_ptr++;
-		}
-		else
-		{
-			break;
+			flashing_addr_ptr = ((tPage*)flashing_addr_ptr)->data_entries_arr;
+			temp_page_entry_itr_ptr = temp_page.data_entries_arr;
+
+			/*Program data entries in the temp page*/
+			Flash_Unlock();
+			while(temp_page_entry_itr_ptr < temp_page_top_entry_ptr)
+			{
+				ret = Flash_Program((uint32_t)flashing_addr_ptr,*((uint32_t*)temp_page_entry_itr_ptr),sizeof(tDataEntry) / 2);
+
+				if(ret == 1)
+				{
+					temp_page_entry_itr_ptr++;
+					flashing_addr_ptr++;
+				}
+				else
+				{
+					break;
+				}
+			}
+			Flash_Lock();
+
+			if(ret == 1)
+			{
+				sectors_runtime_info_arr[sector_no].first_empty_loc_addr = (uint32_t)flashing_addr_ptr;
+			}
 		}
 	}
-	Flash_Lock();
-
-	if(ret == 1)
+	else	/*Handling the case where data was swapped then power goes off*/
 	{
-		sectors_runtime_info_arr[sector_no].first_empty_loc_addr = (uint32_t)flashing_addr_ptr;
+		ret = 1;
 	}
 
 	return ret;
@@ -175,43 +203,35 @@ static uint8_t Emulated_EEPROM_CopyDataToNextPage(uint8_t sector_no,uint8_t page
 static uint8_t Emulated_EEPROM_SwapToNextPage(uint8_t sector_no,uint8_t curr_page_no,tDataEntry *p_entry)
 {
 	uint8_t step_itr = 0;
-	const uint8_t steps_no = 5;
+	const uint8_t steps_no = 3;
 	uint8_t ret = 0;
 	tPageHeader init_page_header;
 	uint8_t next_page_no = GET_NEXT_PAGE_NO(curr_page_no);
 
-	while(step_itr < steps_no &&
-			ret)
+	do
 	{
 		switch(step_itr)
 		{
 		case 0:
-			/*Erase new page to make sure no data was written in it*/
-			ret = Flash_Erase(GET_SECTORS_PAGE_BASE_ADDR(sector_no,next_page_no),1);
-		break;
-		case 1:
 			ret = Emulated_EEPROM_CopyDataToNextPage(sector_no,curr_page_no,p_entry);
 		break;
-		case 2:
+		case 1:
 			init_page_header.page_status = PAGE_STATUS_ACTIVE;
 			ret = Emulated_EEPROM_SetPageHeader(sector_no,next_page_no,init_page_header);
 		break;
-		case 3:
+		case 2:
 			sectors_runtime_info_arr[sector_no].active_page_no = next_page_no;
 
 			/*Clean up old page*/
 			ret = Flash_Erase(GET_SECTORS_PAGE_BASE_ADDR(sector_no,curr_page_no),1);
-		break;
-		case 4:
-			init_page_header.page_status = PAGE_STATUS_INACTIVE;
-			ret = Emulated_EEPROM_SetPageHeader(sector_no,curr_page_no,init_page_header);
 		break;
 		default:
 			break;
 		}
 
 		step_itr++;
-	}
+	}while(step_itr < steps_no &&
+			ret);
 
 	return ret;
 }
@@ -226,11 +246,13 @@ static tDataEntry Emulated_EEPROM_GetLatestEntryForLogicalAddr(uint32_t logical_
 	tDataEntry ret = {0xFFFF,0xFFFF};
 	uint8_t sector_no = IDENTIFY_LOGIC_ADDR_SECTOR(logical_addr);
 	uint8_t page_no = sectors_runtime_info_arr[sector_no].active_page_no;
-	tDataEntry *p_entry = (tDataEntry*)(sectors_runtime_info_arr[sector_no].first_empty_loc_addr - sizeof(tDataEntry));
 
-	if(page_no < FLASH_PAGES_PER_SECTOR)
+	if(sectors_runtime_info_arr[sector_no].first_empty_loc_addr &&
+			(page_no < FLASH_PAGES_PER_SECTOR))
 	{
-		while((uint32_t)p_entry >= (GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no) + sizeof(tPageHeader)))
+		tDataEntry *p_entry = (tDataEntry*)(sectors_runtime_info_arr[sector_no].first_empty_loc_addr - sizeof(tDataEntry));
+
+		while((uint32_t)p_entry >= (uint32_t)(((tPage*)GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no))->data_entries_arr))
 		{
 			if(p_entry->logical_addr == logical_addr)
 			{
@@ -284,47 +306,15 @@ static uint8_t Emulated_EEPROM_InitSector(uint8_t sector_no)
 {
 	uint8_t ret = 0;
 	tPageHeader init_page_header;
-	uint8_t page_itr = 0;
-	uint8_t step_itr = 0;
-	const uint8_t steps_no = 3;
 
+	ret = Flash_Erase(GET_SECTORS_PAGE_BASE_ADDR(sector_no,0),FLASH_PAGES_PER_SECTOR);
 
-	while(step_itr < steps_no &&
-			ret)
+	if(ret)
 	{
-		switch(step_itr)
-		{
-		case 0:
-			ret = Flash_Erase(GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_itr),FLASH_PAGES_PER_SECTOR);
-		break;
-		case 1:
-			/*Init first sector page as active*/
-			init_page_header.page_status = PAGE_STATUS_ACTIVE;
-			ret = Emulated_EEPROM_SetPageHeader(sector_no,page_itr,init_page_header);
-
-		break;
-		case 2:
-			page_itr++;
-
-			/*Init remaining sector pages as inactive*/
-			init_page_header.page_status = PAGE_STATUS_INACTIVE;
-			for(; page_itr < FLASH_PAGES_PER_SECTOR; page_itr++)
-			{
-				ret = Emulated_EEPROM_SetPageHeader(sector_no,page_itr,init_page_header);
-
-				if(!ret)
-				{
-					break;
-				}
-			}
-		break;
-		default:
-			break;
-		}
-
-		step_itr++;
+		/*Init first sector page as active*/
+		init_page_header.page_status = PAGE_STATUS_ACTIVE;
+		ret = Emulated_EEPROM_SetPageHeader(sector_no,0,init_page_header);
 	}
-
 	return ret;
 }
 
@@ -353,18 +343,11 @@ void Emulated_EEPROM_init(void)
 				/*if active page was found before*/
 				if(was_active_page_found == 1)
 				{
-					tPageHeader updated_page_header = {.page_status = PAGE_STATUS_INACTIVE};
-
 					/*if wrapped around*/
 					if(active_page_no == 0 &&
 							(page_itr == (FLASH_PAGES_PER_SECTOR - 1)))
 					{
 						ret = Flash_Erase(GET_SECTORS_PAGE_BASE_ADDR(sectors_itr,page_itr),1);
-
-						if(ret)
-						{
-							Emulated_EEPROM_SetPageHeader(sectors_itr,page_itr,updated_page_header);
-						}
 					}
 					else
 					{
@@ -372,7 +355,6 @@ void Emulated_EEPROM_init(void)
 
 						if(ret)
 						{
-							Emulated_EEPROM_SetPageHeader(sectors_itr,active_page_no,updated_page_header);
 							active_page_no = page_itr;
 						}
 					}
@@ -384,8 +366,7 @@ void Emulated_EEPROM_init(void)
 
 				was_active_page_found = 1;
 			}
-			else if(curr_page_header.page_status == PAGE_STATUS_INACTIVE ||
-					curr_page_header.page_status == PAGE_STATUS_ERASED) /*page without header (fully erased)*/
+			else if(curr_page_header.page_status == PAGE_STATUS_ERASED)
 			{
 				/*do nothing*/
 			}
@@ -440,12 +421,13 @@ uint8_t Emulated_EEPROM_WriteHalfWord(const uint32_t logical_addr,const uint16_t
 	tDataEntry entry = {.logical_addr = logical_addr ,.data = half_word};
 	uint8_t sector_no = IDENTIFY_LOGIC_ADDR_SECTOR(logical_addr);
 	uint8_t page_no = sectors_runtime_info_arr[sector_no].active_page_no;
-	tDataEntry *first_empty_loc_ptr = (tDataEntry *)sectors_runtime_info_arr[sector_no].first_empty_loc_addr;
 
-	if((page_no < FLASH_PAGES_PER_SECTOR) &&
-			(uint32_t)first_empty_loc_ptr != UINT32_MAX)
+	if(page_no < FLASH_PAGES_PER_SECTOR)
 	{
-		if(((uint32_t)first_empty_loc_ptr - GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no)) >= FLASH_PAGE_SIZE)
+		tDataEntry *first_empty_loc_ptr = (tDataEntry *)sectors_runtime_info_arr[sector_no].first_empty_loc_addr;
+
+		if(!first_empty_loc_ptr ||
+				((uint32_t)first_empty_loc_ptr - GET_SECTORS_PAGE_BASE_ADDR(sector_no,page_no)) >= FLASH_PAGE_SIZE) /*within page range*/
 		{
 			ret = Emulated_EEPROM_SwapToNextPage(sector_no,page_no,&entry);
 		}
--- emulated_eeprom.h
+++ emulated_eeprom.h
@@ -1,17 +1,34 @@
-/*
- * emulated_eeprom.h
- *
- *  Created on: Aug 24, 2020
- *      Author: Pola
+/**
+ ******************************************************************************
+ * @file           : Emulated EEPROM
+ * @brief          :
+ ******************************************************************************
  */
 
 #ifndef EMULATED_EEPROM_H_
 #define EMULATED_EEPROM_H_
 
+/** @defgroup Emulated_EEPROM_Module
+  * @brief
+  * @{
+  */
+
 #include <stdint.h>
 
+/* Exported functions -------------------------------------------------------*/
+/** @defgroup Emulated_EEPROM_Module_Exported_FunctionsPrototype Emulated_EEPROM_Module_Exported_FunctionsPrototype
+  * @brief Public functions declaration.
+  * @{
+  */
 void Emulated_EEPROM_init(void);
 uint8_t Emulated_EEPROM_ReadHalfWord(const uint32_t logical_addr,uint16_t *const p_half_word);
 uint8_t Emulated_EEPROM_WriteHalfWord(const uint32_t logical_addr,const uint16_t half_word);
 
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
 #endif /* EMULATED_EEPROM_H_ */
